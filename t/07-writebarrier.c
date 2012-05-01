3b4926f5c8b6201ae7200f4d004eec36857730a3
Arne Skjærholt <arnsholt@gmail.com>
2012-05-01 13:20:47 +0200

    Add tests for write-barriering of CArray and CStruct types.

---
+++ t/07-writebarrier.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+
+typedef struct {
+    long *ptr;
+} Structy;
+
+long _deref(long *ptr) {
+    return *ptr;
+}
+
+long *make_ptr() {
+    long *ptr = (long *) malloc(sizeof(long));
+    *ptr = 32;
+    return ptr;
+}
+
+void struct_twiddle(Structy *s) {
+    s->ptr = (long *) malloc(sizeof(long));
+    *(s->ptr) = 9;
+}
+
+void array_twiddle(long **arr) {
+    arr[0] = (long *) malloc(sizeof(long));
+    arr[1] = (long *) malloc(sizeof(long));
+    arr[2] = (long *) malloc(sizeof(long));
+
+    *arr[0] = 1;
+    *arr[1] = 2;
+    *arr[2] = 3;
+}
+
+void dummy(void **arr) {
+    /* dummy */
+}
