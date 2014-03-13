9e6171306197d9ae7bfe210adde27c22996e928f
Jonathan Worthington <jnthn@jnthn.net>
2014-03-13 21:09:40 +0100

    Support compiling test libraries on Moar.

--- t/CompileTestLib.pm
+++ t/CompileTestLib.pm
@@ -6,11 +6,24 @@ sub compile_test_lib($name) is export {
     # doesn't tell us everything we want to know.
     return if $*VM<name> eq 'jvm';
 
-    my $o  = $*VM<config><o>;
-    my $so = $*VM<config><load_ext>;
-    my $c_line = "$*VM<config><cc> -c $*VM<config><cc_shared> $*VM<config><cc_o_out>$name$o $*VM<config><ccflags> t/$name.c";
-    my $l_line = "$*VM<config><ld> $*VM<config><ld_load_flags> $*VM<config><ldflags> " ~
-        "$*VM<config><libs> $*VM<config><ld_out>$name$so $name$o";
+    my ($c_line, $l_line);
+    if $*VM<name> eq 'parrot' {
+        my $o  = $*VM<config><o>;
+        my $so = $*VM<config><load_ext>;
+        $c_line = "$*VM<config><cc> -c $*VM<config><cc_shared> $*VM<config><cc_o_out>$name$o $*VM<config><ccflags> t/$name.c";
+        $l_line = "$*VM<config><ld> $*VM<config><ld_load_flags> $*VM<config><ldflags> " ~
+            "$*VM<config><libs> $*VM<config><ld_out>$name$so $name$o";
+    }
+    elsif $*VM<name> eq 'moar' {
+        my $o  = $*VM<config><obj>;
+        my $so = $*VM<config><dll>.subst('%s', '');
+        $c_line = "$*VM<config><cc> -c $*VM<config><ccshared> $*VM<config><ccout>$name$o $*VM<config><cflags> t/$name.c";
+        $l_line = "$*VM<config><ld> $*VM<config><ldshared> $*VM<config><ldflags> " ~
+            "$*VM<config><ldlibs> $*VM<config><ldout>$name$so $name$o";
+    }
+    else {
+        die "Unknown VM; don't know how to compile test libraires";
+    }
     shell($c_line);
     shell($l_line);
 }
